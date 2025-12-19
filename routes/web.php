5023a85f2638bd0ffe7b679e5bc199e981732c75
Yoshuaan <124230006@student.upnyk.ac.id>
2025-12-19 10:58:36 +0700

    auth fix, katalog

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\BukuController;
+use App\Http\Controllers\PengumumanController;
 
 // ==========================
 // ROUTE HALAMAN PENGUNJUNG
